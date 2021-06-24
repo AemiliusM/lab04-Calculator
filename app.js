fc8f1a3cce1efacfd4be32f91833bcd6d3a63dcd
Aemilius Morgan <aemiliusmorgan@gmail.com>
2021-06-24 14:21:29 -0700

    Working Calculator

--- app.js
+++ app.js
@@ -1,9 +1,62 @@
-// import functions
 
-// reference needed DOM elements
+import { add, subtract, multiply, divide, squareroot } from './calculator.js'
+
+const addNum1 = document.getElementById('add-num1');
+const addNum2 = document.getElementById('add-num2');
+const addAnswer = document.getElementById('add-answer');
+const addBtn = document.getElementById('add-btn');
+
+const subNum1 = document.getElementById('sub-num1');
+const subNum2 = document.getElementById('sub-num2');
+const subAnswer = document.getElementById('sub-answer');
+const subBtn = document.getElementById('sub-btn');
+
+const divideNum1 = document.getElementById('divide-num1');
+const divideNum2 = document.getElementById('divide-num2');
+const divideAnswer = document.getElementById('divide-answer');
+const divideBtn = document.getElementById('divide-btn');
+
+const multiplyNum1 = document.getElementById('multiply-num1');
+const multiplyNum2 = document.getElementById('multiply-num2');
+const multiplyAnswer = document.getElementById('multiply-answer');
+const multiplyBtn = document.getElementById('multiply-btn');
+
+const squarerootXInput = document.getElementById('squareroot-x-input');
+const squarerootButton = document.getElementById('squareroot-button');
+const squarerootResult = document.getElementById('squareroot-result');
 
 
-// set event listeners 
-  // get user input(s)
-  // do any needed work with the value(s)
-  // update DOM to reflect new value(s)
+
+addBtn.addEventListener('click', () => {
+    const add1 = Number(addNum1.value);
+    const add2 = Number(addNum2.value);
+    const sum = add(add1, add2);
+    addAnswer.textContent = sum;
+});
+
+subBtn.addEventListener('click', () => {
+    const sub1 = Number(subNum1.value);
+    const sub2 = Number(subNum2.value);
+    const difference = subtract(sub1, sub2);
+    subAnswer.textContent = difference;
+});
+
+multiplyBtn.addEventListener('click', () => {
+    const multiply1 = Number(multiplyNum1.value);
+    const multiply2 = Number(multiplyNum2.value);
+    const product = multiply(multiply1, multiply2);
+    multiplyAnswer.textContent = product;
+});
+
+divideBtn.addEventListener('click', () => {
+    const divide1 = Number(divideNum1.value);
+    const divide2 = Number(divideNum2.value);
+    const quotient = divide(divide1, divide2);
+    divideAnswer.textContent = quotient;
+});
+
+squarerootButton.addEventListener('click', () =>{
+    const x = Number(squarerootXInput.value);
+    const answer = squareroot(x);
+    squarerootResult.textContent = answer;
+});
--- test/tests.js
+++ test/tests.js
@@ -1,19 +1,42 @@
 // IMPORT MODULES under test here:
-// import { add } from '../calculator.js';
+import { add } from '../calculator.js';
+import { subtract } from '../calculator.js';
+import { multiply } from '../calculator.js';
+import { divide } from '../calculator.js';
 
 const test = QUnit.test;
 
-// name your test by what it is testing
-test('time to test a function', (expect) => {
-    //Arrange
-    // Set up your arguments and expectations
-    const expected = true;
-    
-    //Act 
-    // Call the function you're testing and set the result to a const
-    const actual = true; // use your function here
 
-    //Expect
-    // Make assertions about what is expected versus the actual result
+test('time to test a function', (expect) => {
+    const expected = 5;
+
+    const actual = add(2, 3); 
+    
+    expect.equal(actual, expected);
+});
+
+test('testing subtract function', (expect) => {
+    const expected = 1;
+
+    const actual = subtract(2, 1);
+
+    expect.equal(actual, expected);
+});
+
+test('testing multiply function', (expect) =>{
+    const expected = 4;
+    const actual = multiply(2, 2);
+    expect.equal(actual, expected);
+});
+
+test('testing dividng function', (expect) =>{
+    const expected = 2;
+    const actual = divide(4, 2);
+    expect.equal(actual, expected);
+});
+
+test('testing square root function', (expect) =>{
+    const expected = 2;
+    const actual = squareroot(4);
     expect.equal(actual, expected);
 });
